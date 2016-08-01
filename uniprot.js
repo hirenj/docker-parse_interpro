05b80565411893e266ee62ce73ebe893f7af5c7d
Hiren Joshi <hirenj@gmail.com>
2016-08-01 15:59:49 +0200

    Start putting in TM downloading

--- uniprot.js
+++ uniprot.js
@@ -8,6 +8,7 @@ const zlib = require('zlib');
 const Transform = stream.Transform;
 
 const UNIPROT_IDS_URL = 'http://www.uniprot.org/uniprot/?compress=yes&fil=reference&force=no&format=list&query=organism:';
+const UNIPROT_TRANSMEMBRANE = 'http://www.uniprot.org/uniprot/?compress=yes&query=&format=tab&columns=id,feature(TRANSMEMBRANE)&fil=organism:';
 
 function Filter(wanted, options) {
 
@@ -32,6 +33,28 @@ Filter.prototype._transform = function (obj, enc, cb) {
   cb();
 };
 
+const line_filter = function(filter,stream) {
+  return new Promise(function(resolve) {
+    var lineReader = require('readline').createInterface({
+      input: stream
+    });
+    lineReader.on('line',function(dat) {
+      let row = dat.toString().split('\t');
+      filter.write({ 'acc' : row[0], 'interpro' : row[1], 'start' : parseInt(row[4]), 'end' : parseInt(row[5]) });
+    });
+
+    lineReader.on('close',function() {
+      filter.end();
+    });
+
+    lineReader.on('error',function(err) {
+      reject(err);
+    });
+
+    resolve(filter);
+  });
+};
+
 const get_ids = function(taxid) {
   var gunzip = zlib.createGunzip();
   request({url: UNIPROT_IDS_URL+taxid }).pipe(gunzip);
@@ -46,8 +69,26 @@ const get_ids = function(taxid) {
   });
 };
 
+
+const get_transmembrane = function(taxid) {
+  var gunzip = zlib.createGunzip();
+  request({url: UNIPROT_TRANSMEMBRANE+taxid }).pipe(gunzip);
+  return line_filter(TransmembraneFilter, gunzip);
+};
+
 const get_wanted_ids = function(taxids) {
   return Promise.all(taxids.map(get_ids)).then(function(all_ids) {
+    var result = {};
+    all_ids.forEach(function(list,idx) {
+      list.forEach(function(acc) { result[acc] = taxids[idx]; });
+    });
+    return result;
+  });
+};
+
+const get_transmembranes = function(taxids) {
+  return Promise.all(taxids.map(get_transmembrane)).then(function(all_ids) {
+    // Streams of tms coming in here.
     var result = {};
     all_ids.forEach(function(list,idx) {
       list.forEach(function(acc) { result[acc] = taxids[idx]; });
